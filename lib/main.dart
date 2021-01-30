ff960ea6ee6bb82234f8c2b9ad6d6bd996295636
boonken13 <waynebkken@gmail.com>
2021-01-30 15:41:56 +0000

    Committed: -separate questions to another file(widget) -learn the use of private convention and final

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,63 @@
 import "package:flutter/material.dart";
+import './question.dart';
 // void main() {
 //   runApp(MyApp());
 // }
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      
-    ),);
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
   }
 }
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  var questions = [
+    "What your favourite color?",
+    "What your favourite animal?",
+  ];
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print("Answer 3 Chosen");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
